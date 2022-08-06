c7b68fcd0aca3aa7684323f98daa98deebb3bad5
Chethan Yadav <rockchethan1111@gmail.com>
2022-08-06 23:33:01 +0530

    updated config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
 module.exports = {
-    Bot_Token: "",
-    Bot_Prefix: "!",
-    Bot_embedColot: "RED",
-    Bot_owenerID: ""
+    Bot_Token: process.env.Bot_Token || "",
+    Bot_Prefix: process.env.Bot_Prefix || "!",
+    Bot_embedColor: process.env.Bot_embedColor || "RED",
+    Bot_owenerID: process.env.Bot_owenerID || ""
 }
